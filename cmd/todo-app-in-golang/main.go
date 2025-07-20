5af042dd1adc9811e6361d097f282fd2e6c8025d
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-07-20 20:44:54 +0530

    setup server

--- cmd/todo-app-in-golang/main.go
+++ cmd/todo-app-in-golang/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/suryanshvermaa/todo-app-in-golang/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello todo-app-in-golang")
+	// load config
+	cfg := config.MustLoad()
+	// database setup
+	// setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to the Todo App!"))
+	})
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to start server:", err.Error())
+	}
+
+	fmt.Println("Server is running on", cfg.Addr)
 }
