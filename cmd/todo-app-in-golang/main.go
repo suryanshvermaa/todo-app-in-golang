182567f51ea88d7ddac83b5bab86a7d31f96e199
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-07-20 21:07:42 +0530

    refactor: implement graceful shutdown for the HTTP server

--- cmd/todo-app-in-golang/main.go
+++ cmd/todo-app-in-golang/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/suryanshvermaa/todo-app-in-golang/internal/config"
 )
@@ -22,11 +28,28 @@ func main() {
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
+	fmt.Println("Server is running on", cfg.Addr)
+	done := make(chan os.Signal, 1)
 
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal("Failed to start server:", err.Error())
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	// graceful shutdown
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Failed to start server:", err.Error())
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
 	}
 
-	fmt.Println("Server is running on", cfg.Addr)
+	slog.Info("Server shutdown successfully")
 }
