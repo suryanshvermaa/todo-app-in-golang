aae85083dfd9290e4efb0d54add6f67da8538bf4
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-07-21 12:19:23 +0530

    fix: replace hardcoded response with JsonResponse for todo creation

--- internal/http/handlers/todo/todo.go
+++ internal/http/handlers/todo/todo.go
@@ -3,12 +3,14 @@ package todo
 import (
 	"log/slog"
 	"net/http"
+
+	"github.com/suryanshvermaa/todo-app-in-golang/internal/utils/response"
 )
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		slog.Info("creating todo")
-		w.Write([]byte("creted todo"))
+		response.JsonResponse(w, http.StatusCreated, "todo created successfully", nil)
 	}
 }
