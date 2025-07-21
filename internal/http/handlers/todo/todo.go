a37fdf98b1cadd943aec670d30a9ba9da3051ad8
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-07-21 12:40:24 +0530

    feat: update todo creation handler to decode request body and return created todo

--- internal/http/handlers/todo/todo.go
+++ internal/http/handlers/todo/todo.go
@@ -1,16 +1,32 @@
 package todo
 
 import (
+	"encoding/json"
+	"errors"
+	"io"
 	"log/slog"
 	"net/http"
+
+	"github.com/suryanshvermaa/todo-app-in-golang/internal/types"
 
 	"github.com/suryanshvermaa/todo-app-in-golang/internal/utils/response"
 )
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
+
+		var todo types.Todo
+		err := json.NewDecoder(r.Body).Decode(&todo)
+		if errors.Is(err, io.EOF) {
+			response.JsonResponse(w, 400, "empty body", nil)
+			return
+		}
+		if err != nil {
+			response.JsonResponse(w, 400, err.Error(), nil)
+			return
+		}
+		todo.Id = 1
 		slog.Info("creating todo")
-		response.JsonResponse(w, http.StatusCreated, "todo created successfully", nil)
+		response.JsonResponse(w, http.StatusCreated, "todo created successfully", todo)
 	}
 }
